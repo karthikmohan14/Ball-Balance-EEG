53cc187188bb769ded8a3166e6b0737dfb1e2caf
karthikmohan14 <karthikmkochi@gmail.com>
2019-01-20 19:38:43 +0530

    first commit

---
+++ Assets/Pause.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Pause : MonoBehaviour
+{
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("ball");
+    }
+}
